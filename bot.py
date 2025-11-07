8739545a7f1da8eba25d7c1ac6cbab0b23cac2c7
Albert_Hakobyan <158502473+Zakonoposlushniy@users.noreply.github.com>
2025-11-07 19:02:44 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import os
 import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
@@ -8,7 +9,6 @@ class TelegramBot:
         self.group_chat_id = group_chat_id
         self.application = ApplicationBuilder().token(self.token).build()
 
-        
         logging.basicConfig(
             format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
             level=logging.INFO
@@ -16,26 +16,36 @@ class TelegramBot:
 
         self.application.add_handler(CommandHandler("start", self.start))
         self.application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, self.handle_message))
+        self.application.add_error_handler(self.error_handler)
 
     async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         await update.message.reply_text("Привет! Я бот.")
 
     async def handle_message(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-        await context.bot.copy_message(
-            chat_id=self.group_chat_id,
-            from_chat_id=update.message.chat_id,
-            message_id=update.message.message_id
-        )
+        try:
+            await context.bot.copy_message(
+                chat_id=self.group_chat_id,
+                from_chat_id=update.message.chat_id,
+                message_id=update.message.message_id
+            )
+        except Exception as e:
+            logging.error(f"Ошибка при пересылке: {e}")
 
-        
+    async def error_handler(self, update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
+        logging.error(f"Глобальная ошибка: {context.error}")
 
     def run(self) -> None:
         self.application.run_polling()
 
 if __name__ == '__main__':
-    TOKEN = '8121514569:AAFJR5MNhVLk1ac0kuxu7m7X4CBzo88dMfE'
-    GROUP_CHAT_ID = -5007465480
+    TOKEN = os.getenv("TOKEN")
+    GROUP_CHAT_ID = int(os.getenv("GROUP_CHAT_ID"))
+
+    if not TOKEN or not GROUP_CHAT_ID:
+        raise ValueError("Переменные окружения TOKEN и GROUP_CHAT_ID не заданы")
+
     bot = TelegramBot(TOKEN, GROUP_CHAT_ID)
     bot.run()
+
 
 
